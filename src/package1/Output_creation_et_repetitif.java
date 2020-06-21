ff1af681493884b1bb4b97e50071c98ef4e466a0
goku <abdelhak2478@gmail.com>
2020-06-21 16:09:10 +0100

    maj maniere de chenger les espaces;chagner nom methodes

--- src/package1/Output_creation_et_repetitif.java
+++ src/package1/Output_creation_et_repetitif.java
@@ -10,45 +10,44 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
+
 
 public class Output_creation_et_repetitif {
     public static void creation_sortie(String out, Document doc_but) throws Exception
     {
         doc_but.setXmlStandalone(true);
-        DOMSource ds = new DOMSource(doc_but);
-        StreamResult res = new StreamResult(new File(out));
 
+        DOMSource ds = new DOMSource(doc_but);
+        StringWriter s = new StringWriter();
+        StreamResult res = new StreamResult(s);
         TransformerFactory transform = TransformerFactory.newInstance();
         //Création du transformateur "tr".
         Transformer tr = transform.newTransformer();
-        tr = transformations(tr,out);
+        StreamResult sortie = new StreamResult(s);
+        tr = setProprieties(tr,out);
         tr.transform(ds, res);
         //transformer les  espaces en \t comme dans les sorties du prof et la ligne en plus dans javafx
-        transform_indentation(out);
+        transformSpaceToindentation(s,out);
+
 
     }
 
-    private static void transform_indentation(String out) throws IOException
-    {
-        Path file = Paths.get(out);
-        Charset charset = StandardCharsets.UTF_8;
-        String content = new String(Files.readAllBytes(file), charset);
-        content = content.replaceAll(" {12}<", "\t\t\t<");
-        content = content.replaceAll(" {8}<", "\t\t<");
-        content = content.replaceAll(" {4}<", "\t<");
-        if(out.equals("javafx.xml"))//pour enlever la derniére ligne en trop
-            content = content.replaceAll("</Racine>\n", "</Racine>");
-        Files.write(file, content.getBytes(charset));
+    private static void transformSpaceToindentation(StringWriter s, String out) throws IOException {
+
+
+        String str = (s+"").replaceAll(" {12}<", "\t\t\t<");
+        str = str.replaceAll(" {8}<", "\t\t<");
+        str = str.replaceAll(" {4}<", "\t<");
+        if (out.equals("javafx.xml")){
+            str = str.replaceFirst("[\n\r]+$", "");
+        }
+        FileOutputStream fos = new FileOutputStream(new File(out));
+        fos.write(str.getBytes("UTF-8"));
+
     }
 
-    private static Transformer transformations (Transformer tra,String out)
+    private static Transformer setProprieties(Transformer tra, String out)
     {
         tra.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
         tra.setOutputProperty(OutputKeys.INDENT, "yes");
